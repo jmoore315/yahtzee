12f4062608f365e24406d27b6aaf5bfc0815fa55
Justin Moore <jmoore315@gmail.com>
2014-02-01 23:49:22 -0600

    Created functions for checking what poker hands dice set matches

--- dice_set.cc
+++ dice_set.cc
@@ -41,3 +41,136 @@ void DiceSet::set_die(int n, int val){
 void DiceSet::deactivate_die(int n){
 	dice[n].value = 0;
 }
+
+int DiceSet::value_of(int n){
+	int score = 0;
+	for (int i = 0; i < num_dice; i++){
+		if (dice[i].value == n){
+			score += n;
+		}
+	}
+	return score;
+}
+
+int DiceSet::aces(){
+	return value_of(1);
+}
+
+int DiceSet::twos(){
+	return value_of(2);
+}
+
+
+int DiceSet::threes(){
+	return value_of(3);
+}
+
+int DiceSet::fours(){
+	return value_of(4);
+}
+
+int DiceSet::fives(){
+	return value_of(5);
+}
+
+int DiceSet::sixes(){
+	return value_of(6);
+}
+
+int DiceSet::three_of_kind(){
+	int count = 0;
+	for (int i = 0; i < 3; i++){
+		int value = dice[i].value;
+		count = 1;
+		for(int j = i+1; j<num_dice; j++){
+			if (dice[j].value == value){
+				count++;
+			}
+		}
+		if(count >= 3)
+			return sum_dice();
+	}
+	return 0;
+}
+
+int DiceSet::four_of_kind(){
+	int count = 0;
+	for (int i = 0; i < 2; i++){
+		int value = dice[i].value;
+		count = 1;
+		for(int j = i+1; j<num_dice; j++){
+			if (dice[j].value == value){
+				count++;
+			}
+		}
+		if(count >= 4)
+			return sum_dice();
+	}
+	return 0;
+}
+int DiceSet::yahtzee(){
+	int count = 1;
+	int value = dice[0].value;
+	for(int i = 1; i<num_dice; i++){
+		if (dice[i].value == value){
+			count++;
+		}
+		else
+			return 0;
+	}
+	return 50;
+}
+
+int DiceSet::full_house(){
+	int three_num = three_of_kind()/3;	//0 if set does not include a 3 of a kind
+	if (three_num){
+		int two_num = 0;
+		for(int i = 0; i < num_dice; i++){
+			if(dice[i].value != three_num){
+				if(!two_num)
+					two_num = dice[i].value;
+				else if (two_num != dice[i].value){
+					//no pair present. return 0
+					return 0;
+				}
+			}
+		}
+	}
+	return 25;
+}
+
+int DiceSet::small_straight(){
+	if(contains(4) && contains(3))
+	{
+		if((contains(5) && contains(6)) || (contains(1) && contains(2)) || (contains(2) && contains(5)))
+			return 30;
+	}
+	return 0;
+}
+
+int DiceSet::large_straight(){
+	if(small_straight()){
+		if(contains(5) && contains(2)){
+			if(contains(1) || contains(6)){
+				return 40;
+			}
+		}
+	}
+	return 0;
+}
+
+bool DiceSet::contains(int val){
+	for(int i = 0; i <num_dice; i++)
+		if(dice[i].value == val)
+			return true;
+	return false;
+}
+
+int DiceSet::sum_dice(){
+	int score = 0;
+	for(int i=0; i<num_dice; i++){
+		score += dice[i].value;
+	}
+	return score;
+
+}
--- dice_set.h
+++ dice_set.h
@@ -15,6 +15,27 @@ public:
 	Die * dice;
 	int num_dice;
 	int num_active_dice;
+
+
+	//the following functions return 0 if dice set does not fit the category, returns the score if it does
+	int aces();
+	int twos();
+	int threes();
+	int fours();
+	int fives();
+	int sixes();
+	int three_of_kind();
+	int four_of_kind();
+	int yahtzee();
+	int full_house();
+	int small_straight();
+	int large_straight();
+
+private:
+	int value_of(int);	//returns total value of a number 1..6 in the dice set
+	bool contains(int);	//returns true if dice set contains a die with specified value
+	int sum_dice();
+
 };
 
 #endif
